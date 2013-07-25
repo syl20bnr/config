7fc294f2e74917502f0071ca6ccff37e63ce2f73
syl20bnr <sylvain.benner@gmail.com>
2013-07-25 09:30:32 -0400

    Add install script for chromium applications (could be done with a loop)

---
+++ .chromium-apps/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp -r ../.chromium-apps ~
+sudo ln -s ~/.chromium-apps/freenode /usr/local/bin/freenode
+sudo ln -s ~/.chromium-apps/googlegroups /usr/local/bin/googlegroups
+sudo ln -s ~/.chromium-apps/grooveshark /usr/local/bin/grooveshark
