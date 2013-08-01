1e9ec66c6d15d7f0356d144399a59769304ccf9d
syl20bnr <sylvain.benner@gmail.com>
2013-08-01 15:57:27 -0400

    Add chromium apps and switch to a makefile for installation

--- .chromium-apps/winstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo ./install.sh
-sudo ln -f -s ~/.chromium-apps/stratus-wiki /usr/local/bin/stratus-wiki
-sudo ln -f -s ~/.chromium-apps/virga-jenkins /usr/local/bin/virga-jenkins
-sudo ln -f -s ~/.chromium-apps/virga-portal /usr/local/bin/virga-portal
-
