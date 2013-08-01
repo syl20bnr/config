8f32885fc181a9264ef320e7dceb242cc00163ab
syl20bnr <sylvain.benner@gmail.com>
2013-07-31 22:26:24 -0400

    Add github chromium application.

--- .chromium-apps/install.sh
+++ .chromium-apps/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cp -r ../.chromium-apps ~
 sudo ln -f -s ~/.chromium-apps/freenode /usr/local/bin/freenode
+sudo ln -f -s ~/.chromium-apps/github /usr/local/bin/github
 sudo ln -f -s ~/.chromium-apps/googlegroups /usr/local/bin/googlegroups
 sudo ln -f -s ~/.chromium-apps/grooveshark /usr/local/bin/grooveshark
 sudo ln -f -s ~/.chromium-apps/psytrancepower /usr/local/bin/psytrancepower
