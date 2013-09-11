96296bd742dff5f61df6955354e1852a5046eb73
syl20bnr <sylvain.benner@gmail.com>
2013-09-11 09:13:35 -0400

    Adjust acceleration velocity for G300

--- .mouse_sens.sh
+++ .mouse_sens.sh
@@ -3,7 +3,7 @@
 
 xinput --set-prop "Razer Razer Diamondback Optical Mouse" "Device Accel Constant Deceleration" 5
 
-xinput --set-prop "pointer:Logitech Gaming Mouse G300" "Device Accel Velocity Scaling" 1.4
+xinput --set-prop "pointer:Logitech Gaming Mouse G300" "Device Accel Velocity Scaling" 1.0
 xinput --set-prop "pointer:Logitech Gaming Mouse G300" "Device Accel Constant Deceleration" 2
 
 
