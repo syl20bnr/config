a2e54fbeb0d67bd2e1ad1de523548438c49035ac
syl20bnr <sylvain.benner@gmail.com>
2014-01-05 00:20:45 -0500

    Add fish configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+# ----------------------------------------------------------------------------
+# Environment
+# ----------------------------------------------------------------------------
+set --export EDITOR vi
+
+# ----------------------------------------------------------------------------
+# Prompt
+# ----------------------------------------------------------------------------
+. ~/.config/fish/functions/vi-mode.fish
+
+function fish_user_key_bindings
+  __vi_mode_normal
+end
+
+function fish_prompt -d "Write out the prompt"
+  printf '%s@%s%s%s%s [%s]> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) $vi_mode
+end
