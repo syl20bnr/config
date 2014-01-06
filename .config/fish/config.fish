9bdc5e30794f4d1a1abf54e53525f2a2190c86f6
syl20bnr <sylvain.benner@gmail.com>
2014-01-06 00:04:21 -0500

    Update fish config in order to use oh-my-fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,17 @@
 # ----------------------------------------------------------------------------
+# oh-my-fish config
+# ----------------------------------------------------------------------------
+set fish_path ~/.oh-my-fish
+set fish_theme simplevi
+set vi_mode_default vi_mode_normal
+set fish_plugins autojump vi-mode
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+# ----------------------------------------------------------------------------
 # Environment
 # ----------------------------------------------------------------------------
-set fish_greeting ""
+set fish_greeting
 set --export EDITOR vi
 
-# ----------------------------------------------------------------------------
-# Prompt
-# ----------------------------------------------------------------------------
-. ~/.config/fish/functions/vi-mode.fish
-
-function fish_user_key_bindings
-  __vi_mode_normal
-end
-
-function fish_prompt -d "Write out the prompt"
-  printf '%s@%s%s%s%s [%s]> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) $vi_mode
-end
